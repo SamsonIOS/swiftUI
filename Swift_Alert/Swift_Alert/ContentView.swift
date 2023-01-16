1e6bf3484efbba6b9dc5dde5ca3a30a8bf21d9a0
Sergei <sc.fod@ya.ru>
2023-01-16 17:53:22 +0300

    add Alert and ActionSheet

--- Swift_Alert/Swift_Alert/ContentView.swift
+++ Swift_Alert/Swift_Alert/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var testFirstView = false
+    @State var testSecondView = false
+    @State var yellow = false
+    @State var testActionSheet = false
+    @State var testActionSheetWithButton = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 25, content: {
+            showAlert()
+            showAlertWith()
+            showActionSheet()
+            showActionSheetButton()
+        })
+    }
+    
+    fileprivate func showAlert() -> some View {
+        return Button {
+            self.testFirstView = true
+        } label: {
+            Text("Показать")
+        }.alert(isPresented: $testFirstView) {
+            Alert(title: Text("Пример Alert"))
         }
-        .padding()
+    }
+    
+    fileprivate func showAlertWith() -> some View {
+        return Button {
+            self.testSecondView = true
+        } label: {
+            if !yellow {
+                Text("Показать")
+            } else {
+                Text("Показать")
+                    .foregroundColor(.green)
+            }
+        }.alert(isPresented: $testSecondView) {
+            Alert(title:Text("Пример Alert c 2 кнопками и логикой"),
+                primaryButton: .destructive(Text("Сменить цвет")) {
+                self.yellow = true
+            } , secondaryButton: .cancel())
+        }
+    }
+    
+    fileprivate func showActionSheet() -> some View {
+        return Button {
+            self.testActionSheet = true
+        } label: {
+            Text("Показать")
+        }.actionSheet(isPresented: $testActionSheet) {
+            ActionSheet(title: Text("«Пример ActionSheet»"))
+        }
+    }
+    
+    fileprivate func showActionSheetButton() -> some View {
+        return Button {
+            self.testActionSheetWithButton = true
+        } label: {
+            Text("Показать 4")
+        }.actionSheet(isPresented: $testActionSheetWithButton) {
+            ActionSheet(title: Text("Пример"), message: Text("ActionSheet c кнопками и логикой"), buttons: [.destructive(Text("Показать")) {
+                print("Test print")
+            }, .cancel()])
+        }
     }
 }
 
