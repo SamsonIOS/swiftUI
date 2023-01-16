a1d0790883612d9d4d8bc32f7d197adc1c3befe6
Sergei <sc.fod@ya.ru>
2023-01-16 14:32:47 +0300

    pattern

---
+++ Swift_Alert/Swift_Alert/Swift_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AlertApp.swift
+//  Swift_Alert
+//
+//  Created by coder on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
