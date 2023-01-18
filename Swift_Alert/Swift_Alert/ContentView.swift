9e7b1b6d3237b45c0e374aff7e4c166ec2af6981
Sergei <sc.fod@ya.ru>
2023-01-18 18:51:46 +0300

    add_toggle_switch

--- Swift_Alert/Swift_Alert/ContentView.swift
+++ Swift_Alert/Swift_Alert/ContentView.swift
@@ -8,6 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private enum Constants {
+        static let balanceTitle = "Баланс: "
+        static let cardTitle = "Карта: "
+        static let facialTitle = "Лицевой счет: "
+        static let profileTitle = "Профиль"
+        static let addBalance = "Пополнить баланс"
+        static let chooseBalance = "Выберите баланс зачисления"
+        static let addCard = "Пополнить карту"
+        static let addFacial = "Пополнить лицевой счет"
+        static let succesBalance = "Баланс успешно пополнен!"
+        static let openFacial = "Открыть счет"
+        static let errorTitle = "Ошибка"
+        static let okTitle = "OK"
+        static let failureTitle = "Невозможно создать счет"
+        static let transferFacialToFacial = "Перевод между счетами"
+        static let succesTitle = "Успешно"
+        static let cardToFacial = "С карты на лицевой счет"
+        static let facialToCard = "Со счета на карту"
+        static let spacerHeigth: CGFloat = 500
+        static let offSetTrue: CGFloat = 250
+        static let offSetFalse: CGFloat = 0
+        static let viewCornerRadius: CGFloat = 10
+        static let vstackSpacing: CGFloat = 10
+        static let rub = "руб."
+        static let animationSpring: CGFloat = 0.3
+        static let dampingFraction: CGFloat = 0.9
+        static let blendDuration: CGFloat = 0.4
+    }
+    
     @State var balance = 0
     @State var walletFirst = 0
     @State var walletSecond = 0
@@ -20,45 +50,45 @@ struct ContentView: View {
         VStack {
             ZStack {
                 HStack {
-                    VStack(spacing: 10, content: {
-                        Text("Баланс: \(balance) руб.")
-                        Text("Карта: \(walletFirst) руб.")
-                        Text("Лицевой счет: \(walletSecond) руб.")
+                    VStack(spacing: Constants.vstackSpacing, content: {
+                        Text("\(Constants.balanceTitle)\(balance) \(Constants.rub)")
+                        Text("\(Constants.cardTitle)\(walletFirst) \(Constants.rub)")
+                        Text("\(Constants.facialTitle)\(walletSecond) \(Constants.rub)")
                         Spacer()
                         payBag()
                         extractedFunc()
                         addMoney()
-                        Spacer().frame(height: 500)
+                        Spacer().frame(height: Constants.spacerHeigth)
                     }).padding()
                     Spacer()
                 }
                 Spacer()
-                RoundedRectangle(cornerRadius: 10)
+                RoundedRectangle(cornerRadius: Constants.viewCornerRadius)
                     .fill(.yellow)
-                    .offset(x: isOnToggle ? 250 : 0)
+                    .offset(x: isOnToggle ? Constants.offSetTrue : Constants.offSetFalse)
             }
             Toggle(isOn: $isOnToggle) {
-                Text("Профиль")
+                Text(Constants.profileTitle)
             }.padding()
-        }.animation(.spring(response: 0.3, dampingFraction: 0.9, blendDuration: 0.4), value: isOnToggle)
+        }.animation(.spring(response: Constants.animationSpring, dampingFraction: Constants.dampingFraction, blendDuration: Constants.blendDuration), value: isOnToggle)
     }
     
     fileprivate func payBag() -> some View {
         return Button {
             self.isClick = true
         } label: {
-            Text("Пополнить баланс")
-        }.confirmationDialog("Выберите баланс зачисления", isPresented: $isClick) {
-            Button("Пополнить карту", role: .destructive) {
+            Text(Constants.addBalance)
+        }.confirmationDialog(Constants.chooseBalance, isPresented: $isClick) {
+            Button(Constants.addCard, role: .destructive) {
                 walletFirst += .random(in: 10...5000)
                 balance = walletFirst + walletSecond
             }
-            Button("Пополнить лицевой счет", role: .destructive) {
+            Button(Constants.addFacial, role: .destructive) {
                 walletSecond += .random(in: 10...5000)
                 balance = walletSecond + walletFirst
             }
         } message: {
-            Text("Баланс успешно пополнен!")
+            Text(Constants.succesBalance)
         }
     }
     
@@ -66,11 +96,11 @@ struct ContentView: View {
         return Button {
             self.isOpenBankAccount = true
         } label: {
-            Text("Открыть счет")
-        }.alert(Text("Ошибка"), isPresented: $isOpenBankAccount) {
-            Button("OK", role: .cancel) {}
+            Text(Constants.openFacial)
+        }.alert(Text(Constants.errorTitle), isPresented: $isOpenBankAccount) {
+            Button(Constants.okTitle, role: .cancel) {}
         } message: {
-            Text("Невозможно создать счет")
+            Text(Constants.failureTitle)
         }
     }
     
@@ -78,11 +108,11 @@ struct ContentView: View {
         return Button {
             self.isAddMoney = true
         } label: {
-            Text("Перевод между счетами")
-        }.alert(Text("Успешно"), isPresented: $isAddMoney) {
-            Button("С карты на лицевой счет", role: .destructive) {
+            Text(Constants.transferFacialToFacial)
+        }.alert(Text(Constants.succesTitle), isPresented: $isAddMoney) {
+            Button(Constants.cardToFacial, role: .destructive) {
             }
-            Button("Со счета на карту", role: .destructive) {
+            Button(Constants.facialToCard, role: .destructive) {
             }
         }
     }
